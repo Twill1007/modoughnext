cba91b3bbf0b2dd29acc059d9863e2e78fc1a8e2
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2023-10-30 23:04:40 -0700

    Updated delete option

--- src/components/itemSelector.js
+++ src/components/itemSelector.js
@@ -1,15 +1,20 @@
 import React, { Fragment, useState } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Link from "next/link";
 import { addItemToCart } from "@/state/cart-slice";
+import { removeItemFromCartById } from "@/state/cart-slice";
+import { cartSlice } from "@/state/cart-slice";
 
 function ItemSelector() {
+  const selectCart = (state) => state.cart;
+  const cart = useSelector(selectCart);
   const dispatch = useDispatch();
   const [selectedValue, setSelectedValue] = useState("Select");
 
   const handleDropdownChange = (event) => {
     setSelectedValue(event.target.value);
   };
+  console.log(cart);
 
   const handleSubmit = () => {
     if (selectedValue !== "Select") {
@@ -20,7 +25,11 @@ function ItemSelector() {
       dispatch(addItemToCart(item));
     }
   };
-  console.log(selectedValue);
+
+  const handleRemoveItem = () => {
+    dispatch(removeItemFromCartById(selectedValue));
+  };
+
   return (
     <Fragment>
       <select value={selectedValue} onChange={handleDropdownChange}>
@@ -28,10 +37,17 @@ function ItemSelector() {
         <option value="1Dozen">One Dozen for $10</option>
         <option value="2Dozen">Two Dozen for $18</option>
       </select>
+      <button onClick={handleSubmit}>Complete Order</button>
       <div>
-        <button onClick={handleSubmit}>Submit</button>
+        {cart.map((item) => (
+          <div key={item.id}>{item.title}</div>
+        ))}
       </div>
+      <button onClick={() => handleRemoveItem(selectedValue)}>
+        Delete Item
+      </button>
       <Link href="/cookie-home">Home</Link>
+      {selectedValue === "select" && <div>{selectedValue}</div>}
     </Fragment>
   );
 }
