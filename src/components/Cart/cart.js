d9c9a0bf8df3241ca85904829a2ca6fc91b58e7a
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2024-01-24 21:30:54 -0700

    Cart CSS

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -13,27 +13,20 @@ function CartSummary() {
     <Fragment>
       <div className="cart-order-summary-container">
         <div className="cart-flex-container">
-          <div className="cart-flex-container-column-1">
-            {items.map((item) => (
-              <div
-                style={{ margin: "20px" }}
-                key={item.productId}
-                className={`cart cookie${item.id}`}
-              ></div>
-            ))}
-          </div>
-          <div className="cart-flex-container-column-2-title">
-            {items.map((item) => (
-              <div key={item.productId}>
-                {item.label} {item.title}
-              </div>
-            ))}
-          </div>
-          <div className="cart-flex-container-column-3">
-            {items.map((item) => (
-              <div key={item.productId}>${item.price}</div>
-            ))}
-          </div>
+          {items.length > 0 ? (
+            <>
+              {items.map((item) => (
+                <div key={item.productId} className="cart-flex-container-row">
+                  <div className={`cart cookie${item.id}`}></div>
+                  <div>{item.label}</div>
+                  <div>{item.title}</div>
+                  <div>${item.price}</div>
+                </div>
+              ))}
+            </>
+          ) : (
+            <div>No items in the cart</div>
+          )}
         </div>
         <div className="cart-total-price">Total Price: ${totalPrice}</div>
       </div>
