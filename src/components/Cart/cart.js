4046bc4a0516187164b1a06bc77aaa8898d2f825
Twill1007 <85330522+Twill1007@users.noreply.github.com>
2024-01-26 19:30:38 -0700

    Cart Edit Buttons

--- src/components/Cart/cart.js
+++ src/components/Cart/cart.js
@@ -1,40 +1,54 @@
-"use client";
-import React, { Fragment } from "react";
+import React, { Fragment, useState } from "react";
 import useCart from "@/hooks/use-cart";
 import "../Cart/cart.css";
 
 function CartSummary() {
   const { items } = useCart();
+  const [editMode, setEditMode] = useState(false);
 
   const itemPrices = items.map((price) => price.price);
   const totalPrice = itemPrices.reduce((sum, price) => sum + price, 0);
 
+  const toggleEditMode = () => {
+    setEditMode((prevEditMode) => !prevEditMode);
+  };
+
   return (
     <Fragment>
       <div className="cart-order-summary-container">
-        <div className="cart-flex-container">
-          {items.length > 0 ? (
-            <>
-              {items.map((item) => (
-                <div key={item.productId} className="cart-flex-container-row">
-                  <div className={`cart cookie${item.id}`}></div>
-                  <div>{item.label}</div>
-                  <div>{item.title}</div>
-                  <div>${item.price}</div>
-                </div>
-              ))}
-            </>
-          ) : (
-            <div>No items in the cart</div>
-          )}
-        </div>
+        {items.length > 0 ? (
+          // <div className={`cart-flex-container ${editMode ? "edit-mode" : ""}`}>
+          <div className="cart-flex-container">
+            {items.map((item) => (
+              <div key={item.productId}>
+                {editMode ? (
+                  <div className="cart-flex-container-row-edit-mode">
+                    <div className={`cart cookie${item.id}`}></div>
+                    <div>{item.label}</div>
+                    <div>{item.title}</div>
+                    <div>${item.price}</div>
+                  </div>
+                ) : (
+                  <div className="cart-flex-container-row">
+                    <div className={`cart cookie${item.id}`}></div>
+                    <div>{item.label}</div>
+                    <div>{item.title}</div>
+                    <div>${item.price}</div>
+                  </div>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div>No items in the cart</div>
+        )}
         <div className="price-edit-button-container">
           <div className="cart-total-price">Total Price: ${totalPrice}</div>
-          {items.length > 0 && (
-            <div>
-              <button>Edit Cart Items</button>
-            </div>
-          )}
+          <div>
+            <button onClick={toggleEditMode}>
+              {editMode ? "Done Editing" : "Edit Cart Items"}
+            </button>
+          </div>
         </div>
       </div>
     </Fragment>
